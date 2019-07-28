103537eac177751f80829bc11d8ecdb3efbf042d
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-28 15:50:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |call| 
-    call.capitalize
+    puts call.capitalize
 end
 end
 
@@ -14,7 +14,7 @@ def long_planeteer_calls(calls)
   calls.any? {|c| c.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
